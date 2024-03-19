ef4b086eaa1e80b840140f021237afa4f82fd407
zhaojun <88965869+luoqiu12138@users.noreply.github.com>
2024-03-19 16:10:43 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -433,7 +433,7 @@ namespace neopixel {
      * Gets the RGB value of a known color
     */
     //% weight=2 blockGap=8
-    //% blockId="neopixel_colors" 
+    //% blockId="neopixel_colors" block="%color"
     export function colors(color: NeoPixelColors): number {
         return color;
     }
